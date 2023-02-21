cd41047050c4c1f7b34510d776c353b6194278fb
7007singh <shw12021982@gmail.com>
2023-02-21 15:16:39 +0530

    [Shweta]Add: Element is inserted in the queue

--- DataStructureStacksAndQueues/Program.cs
+++ DataStructureStacksAndQueues/Program.cs
@@ -10,16 +10,30 @@ namespace DataStructureStacksAndQueues
     {
         static void Main(string[] args)
         {
-            StackLinkedList stack = new StackLinkedList();
-            stack.Push(56);
-            stack.Push(30);
-            stack.Push(70);
-            stack.Display();
-            while(stack.top != null)
+            Console.WriteLine("Choose any program to execute\n1.Stack operations\n2.Queue operations");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
             {
-                stack.Peek();
-                stack.Pop();
-            }            
+                case 1:
+                    StackLinkedList stack = new StackLinkedList();
+                    stack.Push(56);
+                    stack.Push(30);
+                    stack.Push(70);
+                    stack.Display();
+                    while (stack.top != null)
+                    {
+                        stack.Peek();
+                        stack.Pop();
+                    }
+                    break;
+                case 2:
+                    QueueLinkedList queue = new QueueLinkedList();
+                    queue.Enqueue(56);
+                    queue.Enqueue(30);
+                    queue.Enqueue(70);
+                    queue.Display();
+                    break;
+            }  
             Console.ReadLine();
         }
     }
---
+++ DataStructureStacksAndQueues/QueueLinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureStacksAndQueues
+{
+    internal class QueueLinkedList
+    {
+        public Node front;
+        public Node rear;
+        public void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+            if (rear == null)
+            {
+               front = rear = newNode;
+            }
+            else
+            {
+                rear.next = newNode;
+                rear = newNode;
+            }
+            Console.WriteLine("{0} inserted into Queue", data);
+        }
+        public void Display()
+        {
+            Console.WriteLine("Displaying nodes");
+            if (front == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+            }
+            else
+            {
+                Node temp = front;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
